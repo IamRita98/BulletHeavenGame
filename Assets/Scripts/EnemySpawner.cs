7ed5195de76dd4c11cdbb1cd6b9490bbb52b9c5b
Lashvir <dimagax01@gmail.com>
2025-11-09 17:04:27 -0500

    yuh3

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class EnemySpawner : MonoBehaviour
 {
@@ -10,12 +11,35 @@ public class EnemySpawner : MonoBehaviour
     public GameObject enemyPrefab;
     Camera camera;
     float spawnBuffer = 0.2f;
-    public Vector2 spawnPos;
-   
+    Vector2 spawnPos;
+    ObjectPooling oPool;
+    [SerializeField] int enemiesToSpawn = 10;
+    float timer = 0;
+    int scalingDiff = 0;
+    float timeToNextWave = 5;
     void Start()
     {
+        oPool = gameObject.GetComponentInParent<ObjectPooling>();
         camera = Camera.main.GetComponent<Camera>(); //(0,0)bot left and (1,1) top right
-        for (int i = 0; i < 20; i++)
+        print(oPool);
+        SpawnFunc();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= timeToNextWave)
+        {
+            SpawnFunc();
+            timer = 0;
+        }
+        
+    }
+    void SpawnFunc()
+    {
+        int enemiesPerWave = enemiesToSpawn+scalingDiff;
+        for (int i = 0; i < enemiesToSpawn+scalingDiff; i++)
         {
             int r1 = Random.Range(1, 5);
             if (r1 == 1)
@@ -43,15 +67,14 @@ public class EnemySpawner : MonoBehaviour
 
             }
 
-            Instantiate(enemyPrefab, spawnPos, Quaternion.identity);
-        }
-        
-       
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            GameObject t = oPool.objectPool[0];
+            t.transform.position = spawnPos;
+            t.SetActive(true);
+            oPool.objectPool.Remove(t);
+            enemiesPerWave--;
+
+        }
+        scalingDiff++;
     }
 }
---
+++ Assets/Scripts/ObjectPooling.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooling : MonoBehaviour
+{
+    [SerializeField] GameObject poolObject;
+    [SerializeField] int objectAmount=1;
+    public List<GameObject> objectPool=new List<GameObject>();
+
+    void Start()
+    {
+        for (int i = 0; i < objectAmount; i++)
+        {
+            GameObject spawnedGO = Instantiate(poolObject);
+            objectPool.Add(spawnedGO);
+            spawnedGO.SetActive(false);
+        }    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

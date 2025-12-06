b7488d92f14884cef65ae585917576b883e3ae02
Lashvir <dimagax01@gmail.com>
2025-12-06 15:06:32 -0500

    Alex please continue

--- Assets/Scripts/GameManagers/UIManager.cs
+++ Assets/Scripts/GameManagers/UIManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,13 +9,62 @@ public class UIManager : MonoBehaviour
     BaseStats playerBStats;
     BaseWeaponStats baseWeaponStats;
     public List<Button> upgradeButtonList;
+    public enum UpgradeTypes
+    {
+        weapDam,
+        weapFireRate,
+        weapArea,
+        healt,
+        projectiles,
+        globalDam,
+    }
     void Start()
     {
         playerBStats = GameObject.FindGameObjectWithTag("Player").GetComponent<BaseStats>();
         baseWeaponStats = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<BaseWeaponStats>();
     }
+    public class UpgradeInfo
+    {
+        public Dictionary<int,string> descriptions;
+        public Func<int> GetTier;
+    }
+    private Dictionary<UpgradeTypes, UpgradeInfo> upgrades;
+    private void InitializeUpgrades()
+    {
+        upgrades = new Dictionary<UpgradeTypes, UpgradeInfo>
+        {
+            {
+                UpgradeTypes.weapDam,
+                new UpgradeInfo{
+                    GetTier = () => baseWeaponStats.bDamUpgT,
+                    descriptions = new Dictionary<int, string>
+                    {
+                        { 0,"+5% weapon damage upgrade!"},
+                        {1,"+10%" },
+                        {2,"+15% and +1 projectile!" }
+                    }
+                }
+
+            },
+            {
+                UpgradeTypes.weapFireRate,
+                new UpgradeInfo
+                {
+                    GetTier = () => baseWeaponStats.attackRUpgT,
+                    descriptions= new Dictionary<int, string>
+                    {
+                        {0,"+5% fire rate upgrade!" },
+                        {1,"+10% fire rate upgrade!" },
+                        {2,"15% fire rate and +1 projectile!" }
+                    }
+                }
+            },
+            //add more here
+        };
+    }
     public void GetUpgradeInfo(string upgrade)
     {
+
         int tier;
         switch (upgrade)
         {

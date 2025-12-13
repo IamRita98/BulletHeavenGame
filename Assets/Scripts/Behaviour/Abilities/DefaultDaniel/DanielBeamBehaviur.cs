e101effc596fd31e31c0fe29d7cd1104dd10c04f
Lashvir <dimagax01@gmail.com>
2025-12-13 16:48:14 -0500

    needToBugFixBeamStats

--- Assets/Scripts/Behaviour/Abilities/DefaultDaniel/DanielBeamBehaviur.cs
+++ Assets/Scripts/Behaviour/Abilities/DefaultDaniel/DanielBeamBehaviur.cs
@@ -9,18 +9,24 @@ public class DanielBeamBehaviur : MonoBehaviour
     AbilityStats abilityStats;
     bool foundNearestEnemy;
     float timer;
+    BoxCollider2D beamBox;
+    SpriteRenderer beamRenderer;
 
+    public static event System.Action<float, GameObject> OnAttackHit;
     private void Start()
     {
         trackNearestEnemy = GameObject.FindGameObjectWithTag("Player").GetComponent<TrackNeareastEnemy>();
         parentGO = gameObject.transform.parent.gameObject;
         abilityStats = GetComponent<AbilityStats>();
-        gameObject.SetActive(false);
+        //gameObject.SetActive(false);
+        beamBox = gameObject.GetComponent<BoxCollider2D>();
+        beamRenderer = gameObject.GetComponent<SpriteRenderer>();
+        beamBox.enabled = false;
+        beamRenderer.enabled = false;
     }
 
     private void OnEnable()
     {
-        
     }
 
     private void OnDisable()
@@ -31,15 +37,17 @@ public class DanielBeamBehaviur : MonoBehaviour
     private void Update()
     {
         timer += Time.deltaTime;
-        if (!foundNearestEnemy) 
+        if (!foundNearestEnemy)
         {
             foundNearestEnemy = true;
             FindNearestEnemy();
-        } 
+        }
 
-        if(timer >= abilityStats.LifeTime.StatsValue())
+        if (timer >= abilityStats.LifeTime.StatsValue())
         {
             timer = 0;
+            beamBox.enabled = false;
+            beamRenderer.enabled = false;
             gameObject.SetActive(false);
         }
     }
@@ -48,7 +56,12 @@ public class DanielBeamBehaviur : MonoBehaviour
     {
         Vector2 targetPos = trackNearestEnemy.NearestEnemy().transform.position - gameObject.transform.parent.position;
         parentGO.transform.right = targetPos;
-        print(parentGO);
-        print(trackNearestEnemy.NearestEnemy());
+        beamBox.enabled = true;
+        beamRenderer.enabled = true;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.gameObject.CompareTag("Enemy")) return;
+        OnAttackHit?.Invoke(abilityStats.BaseDamage.StatsValue(), collision.gameObject);
     }
 }
--- Assets/Scripts/GameManagers/AbilityManager.cs
+++ Assets/Scripts/GameManagers/AbilityManager.cs
@@ -14,6 +14,16 @@ public class AbilityManager : MonoBehaviour
     GameObject ability3;
     TrackNeareastEnemy trackNearestEnemy;
     GameObject nearestEnemy;
+    GameObject target;
+    float ability1CoolDown;
+    float ability2CoolDown;
+    float ability3CoolDown;
+    bool ability1OnCoolDown=false;
+    bool ability2OnCoolDown;
+    bool ability3OnCoolDown;
+    float ability1Timer;
+    float ability2Timer;
+    float ability3Timer;
 
     private void Awake()
     {
@@ -24,28 +34,66 @@ public class AbilityManager : MonoBehaviour
         ability1 = GameObject.FindGameObjectWithTag("Ability1");
         ability2 = GameObject.FindGameObjectWithTag("Ability2");
         ability3 = GameObject.FindGameObjectWithTag("Ability3");
+        target = GameObject.FindGameObjectWithTag("Target");
+        ability1CoolDown = ability1.GetComponent<AbilityStats>().Cooldown.StatsValue();
+        //ability2CoolDown = ability2.GetComponent<AbilityStats>().Cooldown.StatsValue();
+        //ability3CoolDown = ability3.GetComponent<AbilityStats>().Cooldown.StatsValue();
     }
-
+    private void Update()
+    {
+        
+        if (ability1OnCoolDown)
+        {
+            ability1Timer += Time.deltaTime;
+        }
+        if (ability1Timer >= ability1CoolDown)
+        {
+            ability1Timer = 0f;
+            ability1OnCoolDown = false;
+        }
+        if (ability2OnCoolDown)
+        {
+            ability2Timer += Time.deltaTime;
+        }
+        if (ability2Timer >= ability2CoolDown)
+        {
+            ability2Timer = 0f;
+            ability2OnCoolDown = false;
+        }
+        if (ability3OnCoolDown)
+        {
+            ability3Timer += Time.deltaTime;
+        }
+        if (ability3Timer >= ability3CoolDown)
+        {
+            ability3Timer = 0f;
+            ability3OnCoolDown = false;
+        }
+    }
     public void Ability1()
     {
-        FindNearestEnemy();
+        if (ability1OnCoolDown) return;
+        print("ability CD: "+ability1CoolDown);
         ability1.SetActive(true);
+        ability1OnCoolDown = true;
     }
 
     public void Ability2()
     {
-        FindNearestEnemy();
+        //if (ability2OnCoolDown) return;
+
+        //ability2.SetActive(true);
+        //ability2OnCoolDown = true;
     }
 
     public void Ability3()
     {
-        FindNearestEnemy();
+        //if (ability3OnCoolDown) return;
+
+        //ability3.SetActive(true);
+        //ability3OnCoolDown = true;
     }
 
-    void FindNearestEnemy()
-    {
-        nearestEnemy = trackNearestEnemy.NearestEnemy();
-    }
 }
 
 public class DefaultDanielAbilities

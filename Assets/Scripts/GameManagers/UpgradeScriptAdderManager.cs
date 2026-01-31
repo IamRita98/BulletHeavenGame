b6761c085d45fafc70420b9e300a5f0691f92190
Lashvir <dimagax01@gmail.com>
2026-01-31 13:44:46 -0500

    GOAlexGo

---
+++ Assets/Scripts/GameManagers/UpgradeScriptAdderManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeScriptAdderManager : MonoBehaviour
+{
+    GameObject player;
+    GameObject ability1;
+    GameObject ability2;
+    GameObject ability3;
+    private void Awake()
+    {
+        
+    }
+    void AddBeamRotationUpgrade()
+    {
+
+    }
+}
